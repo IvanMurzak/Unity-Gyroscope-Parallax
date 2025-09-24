13524f1d0d58cfa71bed70342b4c07a47ae4b8c7
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-09-24 01:02:31 -0700

    Bump version number to 1.5.0 in Installer and update download links in README

--- Installer/Assets/com.IvanMurzak/Gyroscope Parallax Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/Gyroscope Parallax Installer/Installer.cs
@@ -15,7 +15,7 @@ namespace com.IvanMurzak.Unity.Gyroscope.Parallax.Installer
     public static partial class Installer
     {
         public const string PackageId = "extensions.unity.gyroscope.parallax";
-        public const string Version = "1.4.1";
+        public const string Version = "1.5.0";
 
         static Installer()
         {
